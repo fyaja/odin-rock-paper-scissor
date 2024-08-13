5bac77df8b9916347a5bd10a4f5756b1d7366531
Ahmad Fay Baihaqi <ahmadfaybaihaqi@gmail.com>
2024-08-13 14:44:16 +0700

    add javascript

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 let humanScore = 0;
 let computerScore = 0;
 
+const buttons = document.querySelectorAll(".buttons button");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    playGame(button.value);
+  });
+});
+
 function getComputerChoice() {
   const chance = Math.random();
   if (chance <= 0.33) {
@@ -12,48 +19,44 @@ function getComputerChoice() {
   }
 }
 
-function getHumanChoice() {
-  let humanChoice = prompt("Choose rock, paper, or scissors!");
-  // using while loops to check the valid input.
-  while (!["rock", "paper", "scissors"].includes(humanChoice.toLowerCase())) {
-    humanChoice = prompt(
-      "Invalid choice! Please choose rock, paper, or scissors:"
-    );
-  }
-
-  return humanChoice.toLowerCase();
-}
-
 function playRound(humanChoice, computerChoice) {
+  const resultText = document.querySelector(".result > p");
   if (humanChoice === computerChoice) {
-    alert(`Tie! ${humanChoice}, ${computerChoice}`);
+    resultText.innerText = `Tie! ${humanChoice}, ${computerChoice}`;
   } else if (
     (humanChoice === "rock" && computerChoice === "scissors") ||
     (humanChoice === "paper" && computerChoice === "rock") ||
     (humanChoice === "scissors" && computerChoice === "paper")
   ) {
-    alert(`You Win! ${humanChoice} beats ${computerChoice}`);
+    resultText.innerText = `You Win! ${humanChoice} beats ${computerChoice}`;
     humanScore++;
+    updateScores();
   } else {
-    alert(`You Lose! ${computerChoice} beats ${humanChoice}`);
+    resultText.innerText = `You Lose! ${computerChoice} beats ${humanChoice}`;
     computerScore++;
+    updateScores();
   }
 }
 
-function playGame() {
-  for (let i = 1; i <= 5; i++) {
-    const humanChoice = getHumanChoice();
-    const computerChoice = getComputerChoice();
-    playRound(humanChoice, computerChoice);
-  }
-
-  if (humanScore > computerScore) {
-    alert(`You Win! ${humanScore} - ${computerScore}`);
-  } else if (computerScore > humanScore) {
-    alert(`You Lose! ${humanScore} - ${computerScore}`);
-  } else {
-    alert(`Tie ! ${humanScore} - ${computerScore}`);
-  }
+function playGame(humanChoice) {
+  const computerChoice = getComputerChoice();
+  playRound(humanChoice, computerChoice);
+  updateImage(humanChoice, computerChoice);
 }
 
-// playGame();
+function updateScores() {
+  document.querySelector(".player-score").innerText = `Player: ${humanScore}`;
+  document.querySelector(
+    ".computer-score"
+  ).innerText = `Computer: ${computerScore}`;
+}
+
+function updateImage(humanChoice, computerChoice) {
+  document.querySelector(".result-img").innerHTML = `
+    <img src="images/${humanChoice}.jpg" alt="${humanChoice}">
+    <span>:</span>
+    <img src="images/${computerChoice}.jpg" alt="${computerChoice}">
+  `;
+}
+
+updateScores();
